612313e357725e04a19a181bf833eb67f3a09123
ElizavetaPetrovskaia <114494434+ElizavetaPetrovskaia@users.noreply.github.com>
2022-11-21 01:25:39 +0100

    Initial Commit

---
+++ FinallyReminders/FinallyRemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinallyRemindersApp.swift
+//  FinallyReminders
+//
+//  Created by Elizaveta Petrovskaia on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FinallyRemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
